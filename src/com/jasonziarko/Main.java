47de2954f5f0493dfca27dcad2254046bf83bd04
ziarkoconsulting <32423423+ziarkoconsulting@users.noreply.github.com>
2017-10-16 15:43:54 -0400

    Initial commit

---
+++ src/com/jasonziarko/Main.java
@@ -0,0 +1,13 @@
+package com.jasonziarko;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = '\u00AE';
+
+        System.out.println("The Unicode output for Registered Symbol is: " + myChar);
+
+
+    }
+}
